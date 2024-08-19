c3a322770e719115b6fe8269a52cd2a48597eb30
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-08-19 16:27:38 +0300

    Add layout button-currency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import { CurrencyFormComponent } from './component/currency-form/currency-form.component';
+import { ButtonCurrencyComponent } from './component/button-currency/button-currency.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyFormComponent,
+    ButtonCurrencyComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/button-currency/button-currency.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-currency',
+  templateUrl: './button-currency.component.html',
+  styleUrls: ['./button-currency.component.scss']
+})
+export class ButtonCurrencyComponent {
+
+}
